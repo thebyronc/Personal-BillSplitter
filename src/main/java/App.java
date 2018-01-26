790d88a7b2108408beadf002a20c24e6579643b6
Byron Chang <thebyronc@gmail.com>
2018-01-26 14:02:46 -0800

    add initial structure for app

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,11 @@
+
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
 public class App {
     public static void main(String[] args) {
 
